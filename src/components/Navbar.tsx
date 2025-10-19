f6f32d9a3770706562f13752f2bf65d4825722a2
FarisOsmanbhoy <farisosmanbhoy01@gmail.com>
2025-10-19 13:42:11 +0000

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -98,13 +98,13 @@ const Navbar: React.FC = () => {
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-28 items-center">
-          <Link to="/" className="flex items-center" style={{ width: '60%', minWidth: '560px' }}>
+        <div className="flex justify-between h-20 items-center">
+          <Link to="/" className="flex items-center py-2" style={{ width: '60%', minWidth: '560px' }}>
             <img
               src="https://dqwwhpgkehvajzqsrwbp.supabase.co/storage/v1/object/public/company-logos/company-logos/Company%20Assets/Company%20Logo.png"
               alt="Flipside Specialties"
               className="h-auto w-full object-contain"
-              style={{ maxHeight: '220px' }}
+              style={{ maxHeight: '64px' }}
             />
           </Link>
 
